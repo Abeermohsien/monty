6e8a466698072085c25aaa42731a30ac22a043ea
Zamoo6 <a.ata.e.001@gmail.com>
2023-12-13 08:26:47 -0800

    &

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * _nop - NOTHING TO DO
+ * @h: head of the stack
+ * @c: line number
+ * Return: void
+ */
+
+void _nop(stack_t **h, unsigned int c)
+{
+	(void) c;
+	(void) h;
+}
---
+++ pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _pop - print top of the stack
+ * @h: head of the stack
+ * @c: line number
+ * Return: void
+ */
+
+void _pop(stack_t **h, unsigned int c)
+{
+	stack_t *hh;
+
+	if (*h == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", c);
+		fclose(st.file);
+		free(st.content);
+		free_stack(*h);
+		exit(EXIT_FAILURE);
+	}
+	hh = *h;
+	*h = hh->next;
+	free(hh);
+}
