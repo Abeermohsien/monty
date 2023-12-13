5be995e156763a381fca20f0832c2d9f8839ede0
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-13 09:17:15 -0800

    $

---
+++ execution.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+/**
+ * _exe - execution
+ * @h: head
+ * @c: line number
+ * @f: monty fule pointer
+ * @content: line content
+ * Return: int
+ */
+int _exe(char *content, stack_t **h, unsigned int c, FILE *f)
+{
+	instruction_t opst[] = {
+		{"push", _push},
+		{"pall", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rot1", _rot1},
+		{"rot2", _rot2},
+		{"queue", _queue},
+		{"stack", _print_top},
+		{NULL, NULL}
+	};
+	unsigned int j = 0;
+	char *oper;
+
+	oper = strtok(content, " \n\t");
+	if (oper && oper[0] == '#')
+		return (0);
+	st.arg =
+		strtok(NULL, " \n\t");
+	while (opst[i].opcode && oper)
+	{
+		if (strcmp(oper, opst[i].opcode) == 0)
+		{
+			opst[i].f(h, c);
+			return (0);
+		}
+		i++;
+	}
+	if (oper && opst[i].opcode == NULL)
+	{
+		fprintf (stderr, "L%d: unkown instruction %s\n", c, oper);
+		fclose(f);
+		free(content);
+		free_stack(*h);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
+}
---
+++ main.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+st_t st = {NULL, NULL, NULL, 0};
+/**
+ * main - interpreter
+ * @ac: arguments number
+ * @av: location of monty file
+ * Return: 0
+ */
+int main(int ac, char *av[])
+{
+	char *c;
+	FILE *file;
+	size_t s = 0;
+	ssize_t r = 1;
+	stack_t *sk = NULL;
+	unsigned int cr = 0;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(av[1], "r");
+	st.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (r > 0)
+	{
+		c = NULL;
+		r = getline(&c, &s, file);
+		st.content = c;
+		c++;
+		if (r > 0)
+		{
+			_exe(c, &sk, cr, file);
+		}
+		free (c);
+	}
+	free_stack(sk);
+	fclose(file);
+return (0);
+}
