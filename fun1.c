416df9e269188a78e5d5d8b89f496b97f79fc9ff
Zamoo6 <a.ata.e.001@gmail.com>
2023-12-14 09:42:00 -0800

    &

--- fun1.c
+++ fun1.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * o_file - to openthe file
  * @fn: file name
@@ -6,17 +7,19 @@
  */
 void o_file(char *fn)
 {
-        FILE *df = fopen(fn, "r");
+	FILE *df = fopen(fn, "r");
 
-        if (fn == NULL || df == NULL)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", fn);
-                free_stack();
-                exit(EXIT_FAILURE);
-        }
-        read_f(df);
-        fclose(df);
+	if (fn == NULL || df == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", fn);
+		free_stack();
+		exit(EXIT_FAILURE);
+	}
+	read_f(df);
+	fclose(df);
 }
+
+
 /**
  * read_f - read files
  * @df: file pointer
@@ -24,15 +27,15 @@ void o_file(char *fn)
  */
 void read_f(FILE *df)
 {
-        int n, m = 0;
-        char *f = NULL;
-        size_t length = 0;
+	int n, m = 0;
+	char *f = NULL;
+	size_t length = 0;
 
-        for (n = 1; getline(&f, &length, df) != -1; n++)
-        {
-                m = p_line(f, n, m);
-        }
-        free(f);
+	for (n = 1; getline(&f, &length, df) != -1; n++)
+	{
+		m = p_line(f, n, m);
+	}
+	free(f);
 }
 
 /**
@@ -44,25 +47,26 @@ void read_f(FILE *df)
  */
 int p_line(char *buf, int n, int m)
 {
-        char *op, *val;
-        const char *del = "\n ";
+	char *op, *val;
+	const char *del = "\n ";
 
-        if (buf == NULL)
-        {
-                fprintf(stderr, "Error: malloc failed\n");
-                free_stack();
-                exit(EXIT_FAILURE};
-        }
-        op = strtok(buf, del);
-        if (op == NULL)
-                return (m);
-        val = strtok(NULL, del);
+	if (buf == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack();
+		exit(EXIT_FAILURE
+				};
+				}
+				op = strtok(buf, del);
+				if (op == NULL)
+				return (m);
+				val = strtok(NULL, del);
 
-        if (strcmp(op, "stack") == 0)
-                return (0);
-        if (strcmp(op, "queue") == 0)
-                return (1);
+				if (strcmp(op, "stack") == 0)
+				return (0);
+				if (strcmp(op, "queue") == 0)
+				return (1);
 
-        _exe(op, val, n, m);
-        return (m);
-}
+				_exe(op, val, n, m);
+				return (m);
+				}
